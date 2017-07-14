bc75274c42d81625815966a8d132ab607a2aeb6c
Maksim P <max@maksimize.com>
2017-07-14 14:49:09 -0700

    Update pub generation.

--- pandapack/assets/generate_pub.sh
+++ pandapack/assets/generate_pub.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
 echo "<<<<< Generating Public SSH Key >>>>>" \
-&& mkdir ~/.ssh \
-&& ssh-keygen -t rsa -C "pandapack" -f "~/.ssh/id_rsa" -P "" \
+&& mkdir -p ~/.ssh \
+&& ssh-keygen -t rsa -C "pandapack" -f ~/.ssh/id_rsa -P "" \
 && cp ~/.ssh/id_rsa.pub ~/Desktop/public_key.txt \
 && echo "<<<<< Your Public Key is on your Desktop >>>>" \
 && read -n1 -r -p "Press Any Key to exit..." key \
